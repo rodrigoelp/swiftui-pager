a3d17711a6e496839f3c4e78f72c4f4e6f08375c
Rod Landaeta <rodrigoelp@gmail.com>
2020-06-15 10:37:04 +1000

    Documented the library before getting some feedback.

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# Pager
+# Pager - Allows you to host multiple pages of content in a pure SwiftUI component.
 
-Simple paging control to be used in Swift UI projects without having to depend on the one provided by UIKit.
+Simple paging control to be used in SwiftUI projects without having to depend on the one provided by UIKit.
+
+## Notes
+It supports iOS 13, watchOS 6 and macOS 10.15. Haven't tested on tvOS device.
+
+# Sample
+
+Let's imagine you have created three different views you want to display (let's say onboarding purposes or a watchOS app).
+
+```swift
+struct Page1View: View {
+    var body: some View {
+        VStack(alignment: .top) { 
+            Text("Here goes the first page!")
+            Spacer()
+            Text("That's all...")
+            Spacer()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct Page2View: View {
+    var body: some View {
+        Text("Nothing fancy for a second page.")
+    }
+}
+
+struct Page3View: View {
+    var body: some View {
+        Text("Yet another simple page...")
+    }
+}
+```
+
+To host all of these views you will need to create a pager and specify each of the views you need.
+
+```swift
+struct HostView: View {
+    var body: some View {
+        Pager {
+            [ // The pager requires a list of views as its view builder.
+                AnyView(Page1View()),
+                AnyView(Page2View()),
+                AnyView(Page3View())
+            ]
+        }
+    }
+}
+```
+
+And that is all, the views will be loaded once the Pager is loaded.
--- Sources/Pager/Pager.swift
+++ Sources/Pager/Pager.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-/// Helper view with not much use for now.
+/// Helper view with not much use for now, but allows me to delay rendering the page as the pager is built.
 private struct Page<Content>: View where Content: View {
     private let content: () -> Content
 
@@ -26,9 +26,17 @@ public struct PagerStyle {
     let maxPageNumber: Int
     let focusedScale: CGFloat
     let unfocusedScale: CGFloat
-
     let animation: Animation
 
+    /// Initializes the style to follow on each element contained by the pager.
+    /// - Parameter interPagePadding: spacing between pages.
+    /// - Parameter maxPage: Used to calculate the maximum page boundary, allows you to indicate how far do you want
+    /// pages when these are not rendered or prepared to be visible.
+    ///  Basically, an optimization parameter.
+    /// - Parameter focusedScale: scale of focused/selected/active pages.
+    /// - Parameter unfocusedScale: scale of unfocused/unselected/inactive pages.
+    /// - Parameter animation: Animation used transitioning between pages (applied when the page snaps in place, to its
+    /// scale and its visual importance).
     public init(interPagePadding: CGFloat = 20,
          pageSize: CGSize = .init(width: 300, height: 500),
          maxPage: Int = 20,
@@ -61,18 +69,141 @@ private extension View {
     }
 }
 
+/// Visual indicator type
+public enum PageIndicatorType {
+    /// I think the page indicator looks awful... don't show it.
+    case none
+    /// This is the default that matches iOS/watchOS
+    case dot
+    /// I want to be different. Square different. Like bobsponge.
+    case square
+}
+
+private extension PageIndicatorType {
+    func asView(size: CGSize) -> AnyView {
+        switch self {
+        case .none:
+            return AnyView(EmptyView())
+        case .dot:
+            return AnyView(Circle().size(size))
+        case .square:
+            return AnyView(Rectangle().size(size))
+        }
+    }
+}
+
+/// Allows you to style the page indicator.
+public struct PageIndicatorStyle {
+    public static var `default` = PageIndicatorStyle()
+    let type: PageIndicatorType
+    let activeOpacity: Double
+    let inactiveOpacity: Double
+    let foreground: Color?
+    let size: CGSize
+
+    /// Initializes a new style for the page indicator.
+    /// - Parameter activeOpacity: opacity of the active/selected page.
+    /// - Parameter inactiveOpacity: opacity of the inactive/unselected page.
+    /// - Parameter foreground: color of each element in the indicator.
+    /// - Parameter size: size of the each indicator.
+    /// - Parameter type: visual type of the indicator. Choose between .none, dot (circle), or square.
+    /// If you choose .none the entire indicator and every single element within won't be rendered.
+    public init(activeOpacity: Double = 1,
+                inactiveOpacity: Double = 0.4,
+                foreground: Color? = .white,
+                size: CGSize = CGSize(width: 8, height: 8),
+                type: PageIndicatorType = .dot
+    ) {
+        self.activeOpacity = activeOpacity
+        self.inactiveOpacity = inactiveOpacity
+        self.foreground = foreground
+        self.size = size
+        self.type = type
+    }
+}
+
+private struct PageIndicator: View {
+    private let indexes: Range<Int>
+    private let active: Int?
+    private let style: PageIndicatorStyle
+    private let shapeBuilder: () -> AnyView
+
+    init(style: PageIndicatorStyle,
+         indexes: Range<Int>,
+         active: Int?
+    ) {
+        self.indexes = indexes
+        self.active = active
+        self.style = style
+        self.shapeBuilder = { style.type.asView(size: style.size) }
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                HStack(alignment: .center) {
+                    Spacer()
+                    ForEach(self.indexes) { index in
+                        self.shapeBuilder()
+                            .foregroundColor(self.style.foreground)
+                            .opacity(self.active == index ? self.style.activeOpacity: self.style.inactiveOpacity)
+                    }
+                    Spacer()
+                }.frame(height: self.style.size.height + 8, alignment: .top)
+            }.frame(maxWidth: geometry.size.width * 0.6)
+        }
+    }
+}
+
+/// Provides a mechanism to display pages/views.
 public struct Pager<Content>: View where Content: View {
     private let views: [Page<Content>]
     private let style: PagerStyle
+    private let indicatorStyle: PageIndicatorStyle
     @State private var activeWindow: ActiveWindow?
     @GestureState private var dragState = DragState.inactive
 
+    /// Initialises a pager with as many styled pages as indicated.
+    /// - Parameter style: General style used by the pager. Allows you to set visual appearance of
+    /// pages, spacing between pages, animation, etc.
+    /// - Parameter indicatorStyle: General style used by the selected page indicator. It can be used to turn it off
+    /// change color and size of the indicator.
+    /// - Parameter pages: List of views used to populate the pager.
+    /// - Remark:
+    /// An example of how to use the pager would be
+    /// ```swift
+    ///  struct ContentView: View {
+    ///      var body: some View {
+    ///          GeometryReader { geometry in
+    ///              Pager(indicatorStyle: PageIndicatorStyle(type: .dot)) {
+    ///                  [
+    ///                      AnyView(Text("Page 1"))
+    ///                          .frame(width: geometry.size.width, height: geometry.size.height)
+    ///                          .background(.blue),
+    ///                      AnyView(Text("Page 2") )
+    ///                          .frame(width: geometry.size.width, height: geometry.size.height)
+    ///                          .background(.green),
+    ///                      AnyView(Text("Page 3") )
+    ///                          .frame(width: geometry.size.width, height: geometry.size.height)
+    ///                          .background(.orange),
+    ///                      AnyView(Text("Page 4") )
+    ///                          .frame(width: geometry.size.width, height: geometry.size.height)
+    ///                          .background(.yellow),
+    ///                  ]
+    ///              }
+    ///          }
+    ///      }
+    ///  }
+    /// ```
     public init(
         style: PagerStyle = PagerStyle.default,
+        indicatorStyle: PageIndicatorStyle = PageIndicatorStyle.default,
         @ViewBuilder pages: @escaping () -> [Content]
     ) {
         views = pages().map { view in Page { view } }
         self.style = style
+        self.indicatorStyle = indicatorStyle
     }
 
     public var body: some View {
@@ -87,6 +218,16 @@ public struct Pager<Content>: View where Content: View {
                     .animation(self.style.animation)
                     .visibility(visible: self.elementVisibility(pair.offset))
             }
+            Group {
+                if self.indicatorStyle.type != .none {
+                    PageIndicator(style: self.indicatorStyle,
+                                  indexes: 0..<self.views.count,
+                                  active: self.activeWindow?.active)
+                } else {
+                    EmptyView()
+                }
+            }
+            .zIndex(3)
         }.gesture(
             DragGesture()
                 .updating(self.$dragState) { drag, state, _ in
